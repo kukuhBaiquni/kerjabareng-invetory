1cde9d32a89116b980d60b2f5f8d18662449ccb4
aditya wira nugraha <aditya_wira98@yahoo.com>
2018-04-26 13:04:34 +0700

    add models

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+let mongoose = require('mongoose');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,7 +21,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+mongoose.connect('mongodb://localhost/inventory', ()=>{
+  console.log('connect to mongoose')
+})
 app.use('/', index);
 app.use('/users', users);
 
